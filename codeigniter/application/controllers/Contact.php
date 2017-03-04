9bf4f95f3285e07589b67a2c2ef18d6936a15d08
gatorkoco <gatra0013@gmail.com>
2017-03-04 18:47:09 +0700

    Praktikum1_Frameworking

---
+++ codeigniter/application/controllers/Contact.php
@@ -0,0 +1,15 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contact extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('contact');
+	}
+
+}
+
+/* End of file controllername.php */
+/* Location: ./application/controllers/controllername.php */
---
+++ codeigniter/application/views/contact.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="http://localhost/codeigniter/assets/css/bootstrap.min.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Framework</a>
+			</div>
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="<?php echo site_url('home')?>">Home</a></li>
+					<li><a href="<?php echo site_url('about')?>">About</a></li>
+				</ul>
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo site_url('contact')?>">Contact</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+
+		<div class="jumbotron">
+			<div class="container">
+				<p><input type="text" name="Nama" id="inputNama" class="form-control" value="Nama" required="required" pattern="" title=""></p>
+				<p><input type="email" name="Email" id="inputEmail" class="form-control" value="Email" required="required" title=""></p>
+				<p><input type="tel" name="Telpon" id="inputTelpon" class="form-control" value="Telpon" required="required" title=""></p>
+				<button type="button" class="btn btn-default">Submit</button>
+			</div>
+		</div>
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="http://localhost/codeigniter/assets/css/bootstrap.min.css"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
